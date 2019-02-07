e2a961115118a53aa9d9591a60ee56c35ffad4d1
Gaurav <gauravsgandhi@gmail.com>
2019-02-07 18:42:01 +0530

    'node'

---
+++ myfirstmodule.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var dt = require('myfirstmoduledate');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time is currently: " + dt.myDateTime());
+  res.end();
+}).listen(8082);
